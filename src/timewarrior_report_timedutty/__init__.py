705dde508330b8e602c751b5455b7d630f0b8f70
Simon Lundström <github-commits@soy.se>
2020-10-29 15:24:21 +0100

    Not needed and hides problems

--- src/timewarrior_report_timedutty/__init__.py
+++ src/timewarrior_report_timedutty/__init__.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring,missing-function-docstring,import-error
+# pylint: disable=missing-module-docstring,missing-function-docstring
 __version__ = '0.1.0'
 
 import math
