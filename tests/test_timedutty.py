b241507a510e11cf58b32838328fd88905f18af8
Simon Lundström <github-commits@soy.se>
2020-10-29 13:50:58 +0100

    Greenbar!

--- tests/test_timedutty.py
+++ tests/test_timedutty.py
@@ -1,15 +1,12 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring
-from timewarrior_report_timedutty import __time_intervals, __version__
+from timewarrior_report_timedutty import __version__, duration
 
-
-def gurken(*, seconds):
-    return "P1H30M"
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def test_duration_1h_30m():
-    assert "P1H30M" == gurken(seconds=5400)
+    assert "P1H30M" == duration(seconds=5400)
 
 def test_duration_2h_31m():
-    assert "P2H31M" == gurken(seconds=9060)
+    assert "P2H31M" == duration(seconds=9060)
